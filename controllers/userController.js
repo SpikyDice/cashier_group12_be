5f7d9063cc96448601b174ef56625f6255b1e68c
Donny Gozali <gozalidonny@gmail.com>
2023-04-29 12:21:08 +0700

    adding try catch for register

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,46 +5,54 @@ const nodemailer = require(`../helper/nodemailer`);
 
 module.exports = {
   register: async (req, res) => {
-    //destructuring the data
-    const { name, password, email, phone, storeName } = req.body;
+    try {
+      //destructuring the data
+      const { name, password, email, phone, storeName } = req.body;
 
-    //look whether the email exist or not
-    let getEmailQuery = `select * from users where email=${db.escape(email)}`;
-    let isEmailExist = await query(getEmailQuery);
-    if (isEmailExist.length > 0) {
-      return res.status(400).send({ message: "Email already exist" });
+      //look whether the email exist or not
+      let getEmailQuery = `select * from users where email=${db.escape(email)}`;
+      let isEmailExist = await query(getEmailQuery);
+      if (isEmailExist.length > 0) {
+        return res
+          .status(200)
+          .send({ success: false, message: "Email already exist" });
+      }
+
+      //if email don't exist then starts hashing the password
+      let hashpassword = await bcrypt.hash(password, 10);
+
+      //insert user data into sql user database
+      let addUserQuery = `insert into users values (null, ${db.escape(
+        name
+      )}, ${db.escape(hashpassword)}, ${db.escape(email)}, ${db.escape(
+        storeName
+      )}
+        , ${db.escape(phone)}, 0, 0)`;
+      let addUserResult = await query(addUserQuery);
+
+      let payload = {
+        id: addUserResult.insertId,
+      };
+      const token = jwt.sign(payload, "meong", { expiresIn: "4h" });
+
+      let mail = {
+        from: "Admin <gozalidonny@gmail.com>",
+        to: `${email}`,
+        subject: "Account Registration",
+        html: `<div>
+        <p>Click here for verification</p>
+        <a href="http://localhost:3000/user/verification/${token}">Click here</a>
+        </div>`,
+      };
+
+      let response = await nodemailer.sendMail(mail);
+
+      return res
+        .status(200)
+        .send({ success: true, message: "Register Success", addUserResult });
+    } catch (error) {
+      return res.status(400).send(error);
     }
-
-    //if email don't exist then starts hashing the password
-    let hashpassword = await bcrypt.hash(password, 10);
-
-    //insert user data into sql user database
-    let addUserQuery = `insert into users values (null, ${db.escape(
-      name
-    )}, ${db.escape(hashpassword)}, ${db.escape(email)}, ${db.escape(storeName)}
-      , ${db.escape(phone)}, 0, 0)`;
-    let addUserResult = await query(addUserQuery);
-
-    let payload = {
-      id: addUserResult.insertId,
-    };
-    const token = jwt.sign(payload, "meong", { expiresIn: "4h" });
-
-    let mail = {
-      from: "Admin <gozalidonny@gmail.com>",
-      to: `${email}`,
-      subject: "Account Registration",
-      html: `<div>
-      <p>Click here for verification</p>
-      <a href="http://localhost:3000/user/verification/${token}">Click here</a>
-      </div>`,
-    };
-
-    let response = await nodemailer.sendMail(mail);
-
-    return res
-      .status(200)
-      .send({ data: addUserResult, message: "Register Success" });
   },
   login: async (req, res) => {
     try {
